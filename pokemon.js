a9e132fb5b34b2236ca584f55dd37fe6156ef15a
Dev Kauan <144623928+DEVKauanrodriguesguimaraes@users.noreply.github.com>
2023-11-01 17:29:34 -0300

    Upload projeto Pokemon

---
+++ pokemon.js
@@ -0,0 +1,39 @@
+//Armazenar informações do pokemon(Nome, Sexo, Level, Hp)
+let namePokemons = [" Poochvena ", " Zigzagoon ", " Dragonite "," Hurhple "]
+let tiposDePokemon = [ 
+  [" Poochvena ", " Masculino ", 2, 13 ],
+  [" Zigzagoon ", " Feminino ", 2, 13 ],
+  [" Dragonite ", " Masculino ", 5, 25],
+  [" Dragonite ", " Feminino ", 5, 24],
+  [" Dragonite ", " Feminino ", 5, 24],
+  [" Poochvena ", " Feminio ", 3, 15],
+  [" Humhple ", " Masculino ", 2, 7],
+]
+// O pokemon da matriz na linha 3 e 4 são iguais, como o professor pediu para adicionar todos os pokemons e
+//pode ser ,que se fosse um jogo, poderia cadastrar pokemons iguais, estou colocando na matriz.
+
+const nome = "O pokemon"
+const sexo = "é do sexo"
+const level = "está no level "
+const hp = " e tem o HP "
+
+console.log( nome + tiposDePokemon[0][0] + sexo + tiposDePokemon[0][1]
+  + level + tiposDePokemon[0][2] + hp + tiposDePokemon[0][3] + " (Foi cadastrado com sucesso) ") 
+  
+  console.log( nome + tiposDePokemon[1][0] + sexo + tiposDePokemon[1][1]
+  + level + tiposDePokemon[1][2] + hp + tiposDePokemon[1][3] + " (Foi cadastrado com sucesso) ") 
+  
+  console.log( nome + tiposDePokemon[2][0] + sexo + tiposDePokemon[2][1]
+  + level + tiposDePokemon[2][2] + hp + tiposDePokemon[2][3] + " (Foi cadastrado com sucesso) ") 
+  
+  console.log( nome + tiposDePokemon[3][0] + sexo + tiposDePokemon[3][1]
+  + level + tiposDePokemon[3][2] + hp + tiposDePokemon[3][3] + " (Foi cadastrado com sucesso) ") 
+  
+  console.log( nome + tiposDePokemon[4][0] + sexo + tiposDePokemon[4][1]
+  + level + tiposDePokemon[4][2] + hp + tiposDePokemon[4][3] + " (Foi cadastrado com sucesso) ") 
+  
+  console.log( nome + tiposDePokemon[5][0] + sexo + tiposDePokemon[5][1]
+  + level + tiposDePokemon[5][2] + hp + tiposDePokemon[5][3] + " (Foi cadastrado com sucesso) ") 
+  
+  console.log( nome + tiposDePokemon[6][0] + sexo + tiposDePokemon[6][1]
+  + level + tiposDePokemon[6][2] + hp + tiposDePokemon[6][3] + " (Foi cadastrado com sucesso) ") 
